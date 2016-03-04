1f3d89857275aabeede9a13a01a1ad18c5e3948f
A3 <liufengshan66@qq.com>
2016-03-04 17:29:51 +0800

    基本的界面搭建和plist文件的编写
    基本的界面搭建和plist文件的编写

---
+++ SimpleImageBrowser/SimpleImageBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleImageBrowser
+//
+//  Created by mac on 16/3/4.
+//  Copyright © 2016年 A3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
