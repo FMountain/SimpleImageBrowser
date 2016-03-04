751fe3173c0e87b245b8f5c8d637afbfdd4b7436
A3 <liufengshan66@qq.com>
2016-03-04 18:01:09 +0800

    图片浏览器代码实现
    图片浏览器代码实现

--- SimpleImageBrowser/SimpleImageBrowser/ViewController.m
+++ SimpleImageBrowser/SimpleImageBrowser/ViewController.m
@@ -9,10 +9,52 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *topLabel;
+@property (weak, nonatomic) IBOutlet UILabel *descLabel;
+@property (weak, nonatomic) IBOutlet UIButton *leftBtn;
+@property (weak, nonatomic) IBOutlet UIButton *rightBtn;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
+@property (nonatomic,assign)int index; //索引
+
+@property (nonatomic,strong)NSArray *imageDicts;
 @end
 
 @implementation ViewController
+
+#pragma mark - 懒加载plist文件
+- (NSArray *)imageDicts
+{
+    if (!_imageDicts) {
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"imageDesc.plist" ofType:nil];
+        _imageDicts = [NSArray arrayWithContentsOfFile:path];
+    }
+    return  _imageDicts;
+}
+- (IBAction)leftBtn:(UIButton *)sender {
+    self.index --;
+    [self btnClickChange];
+}
+- (IBAction)rightBtn:(UIButton *)sender {
+    self.index ++;
+    [self btnClickChange];
+}
+
+- (void)btnClickChange
+{
+    self.topLabel.text = [NSString stringWithFormat:@"%d/%d",
+                          (self.index + 1),self.imageDicts.count];
+    
+    self.descLabel.text = self.imageDicts[self.index][@"description"];
+    
+    self.imageView.image = [UIImage imageNamed:self.imageDicts[self.index][@"name"]];
+    
+    self.leftBtn.enabled = (self.index != 0);
+    
+    //只要不是最后一张图片都能点击
+    self.rightBtn.enabled = (self.index != self.imageDicts.count - 1);
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
